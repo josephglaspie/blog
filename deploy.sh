ff544824b5d1ac0afe606261ff013eb6f424ca3b
Joseph Glaspie <josephglaspie@gmail.com>
2022-01-29 12:16:31 -0600

    cloned from pub

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 #hugo # if using a theme, replace by `
-hugo -t kiera
+hugo -t hugo-kiera
 
 # Go To Public folder
 cd public
